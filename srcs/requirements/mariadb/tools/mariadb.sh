d4fa59d9991622debf4bc4196bfcc71c1eef1b7e
Markos Vinicius <markos@unknown2edb640aeade.lan>
2025-04-21 11:36:40 +0100

    credentials issue for root

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 echo "This script is being called"
+DB_PASSWORD=$(cat /run/secrets/db_password)
+ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
 # Initialize database
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    # Start MariaDB
+    
+    # Start MariaDB temporarily
     mysqld --user=mysql --bootstrap << EOF
-USE mysql;
+DELETE FROM mysql.user WHERE User='';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 fi
